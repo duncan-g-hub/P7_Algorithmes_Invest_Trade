fb6fb273d17b9ee354ce07ffe73e2137e19c3900
duncan-g-hub <duncan.gaurat@outlook.fr>
2026-02-13 10:56:10 +0100

    Ajout fonction de nettoyage des données csv

--- bruteforce.py
+++ bruteforce.py
@@ -11,3 +11,16 @@ def get_data_from_csv(csv_file):
     with open(DATA_DIR / csv_file, encoding="utf-8") as csvfile:
         reader = csv.DictReader(csvfile, delimiter=',')
         return list(reader)
+
+
+#Nettoyer la liste des actions
+def clean_data(actions):
+    cleaned_actions = []
+    for action in actions:
+        cleaned_action = {
+            "name": action["Actions #"],
+            "cost": int(action["Coût par action (en euros)"]),
+            "profit_percent" : float(action["Bénéfice (après 2 ans)"].replace("%",""))/100
+        }
+        cleaned_actions.append(cleaned_action)
+    return cleaned_actions
