af188046c83beef502d260359fe88677005c9574
duncan-g-hub <duncan.gaurat@outlook.fr>
2026-02-13 14:34:25 +0100

    Mise à jour de la fonction de recursion pour explorer la totalité des combinaisons possibles

--- bruteforce.py
+++ bruteforce.py
@@ -33,59 +33,66 @@ def calculate_profit(actions):
     return actions
 
 
-# trier les actions par pourcentage de bénéfice croissant (meilleurs bénéfices)
-def sort_actions_by_profit(actions):
-    return sorted(actions, key=lambda action: action["profit_percent"])
+# # trier les actions par pourcentage de bénéfice croissant (meilleurs bénéfices)
+# def sort_actions_by_profit(actions):
+#     return sorted(actions, key=lambda action: action["profit_percent"])
+#
+#
+# # récupérer la meilleure combinaison d'action sans dépasser 500€ de budget
+# def get_best_actions(actions):
+#     best_actions = []
+#     total_cost = 0
+#     i = 1
+#     while i <= len(actions) :
+#         if total_cost + actions[-i]["cost"] <= 500:
+#             best_actions.append(actions[-i])
+#             total_cost += actions[-i]["cost"]
+#         i += 1
+#     return best_actions
 
 
-# récupérer la meilleure combinaison d'action sans dépasser 500€ de budget
-def get_best_actions(actions):
-    best_actions = []
-    total_cost = 0
-    i = 1
-    while i <= len(actions) :
-        if total_cost + actions[-i]["cost"] <= 500:
-            best_actions.append(actions[-i])
-            total_cost += actions[-i]["cost"]
-        i += 1
+
+# récupérer la meilleure combinaison d'action en testant toutes les combinaisons sans dépasser 500€ de budget avec récusivité
+# la fonction combinations du module itertools permet de faire la meme chose plus simplement (recursivité pour comprendre)
+def get_best_actions(actions, max_budget=500):
+
+    # on test toutes les combinaison possibles
+    def test_best_actions(index, current_combination, current_cost, current_profit):
+        # On définit les variables meilleure combinaison et meilleur benef = combinaison et benef courrant.
+        # Ce sont des valeurs de référence avec d'explorer la branche de recusivité courante.
+        best_profit = current_profit
+        best_combination = current_combination
+
+        # condition d'arret (si on dépasse le budget)
+        if current_cost > max_budget:
+            return 0, []
+
+
+        # on explore toutes les combinaisons uniques
+        # On boucle sur toutes les actions à partir de l'index courant (evite les doublons et les combinaisons inversées)
+        for i in range(index, len(actions)):
+
+            action = actions[i] # on stock l'action courrante
+
+            # on récupère la combinaison d'actions et le profit correspondant via la recursivité
+            profit, combination = test_best_actions(
+                i+1, # index correspondant à l'action d'aprés (permet de récupérer toutes les combinaisons en excluant l'action courrante)
+                current_combination + [action], # combinaison d'action à laquelle on ajoute l'action courrante, on construit progressivement la combinaison
+                current_cost + action["cost"], # Mise à jour du cout
+                current_profit + action["profit_euro"]) # Mise à jour des benefs
+
+            # si le benef total de la combinaison est > au meilleur benef, on met à jour le best benef et la best combinaison
+            if profit > best_profit:
+                best_profit = profit
+                best_combination = combination
+
+        # on retourne la meilleure combinaison avec le benef total correspondant
+        return best_profit, best_combination
+
+    _, best_actions = test_best_actions(0, [], 0, 0)
+
     return best_actions
 
-
-# # récupérer la meilleure combinaison d'action sans dépasser 500€ de budget avec récusivité
-# def get_best_actions(best_actions, actions, cost):
-#
-#     # condition d'arret (plus aucune action)
-#     if not actions:
-#         return best_actions
-#
-#     # on stock la premiere action dans une varaible
-#     best_action = actions[0]
-#     # on boucle sur le nombre d'action
-#     for i in range(1, len(actions)):
-#         # on stock la prochaine action dans une variable
-#         next_action = actions[i]
-#         # si la prochaine action est mieux que la premiere on la remplace
-#         if next_action["profit_percent"] > best_action["profit_percent"]:
-#             best_action = actions[i]
-#
-#     # on copie la liste des actions
-#     remaining_actions = actions[:]
-#
-#     # si l'action + les couts dépassent le budget alors on l'enleve de remaining actions
-#     if cost + best_action["cost"] > 500:
-#         remaining_actions.remove(best_action)
-#         # on lance la recursivité avec les actions restantes
-#         return get_best_actions(best_actions, remaining_actions, cost)
-#
-#     # sinon on ajoute l'action à best_actions
-#     best_actions.append(best_action)
-#     # on l'enleve de remaining actions
-#     remaining_actions.remove(best_action)
-#     # on recalcul le cout total
-#     cost += best_action["cost"]
-#
-#     # on applique la recusivité en passant les nouvelles listes et le cout total mis à jour
-#     return get_best_actions(best_actions, remaining_actions, cost)
 
 
 # Afficher la liste des meilleures actions, avec le cout total et le bénéfice total aprés 2 ans.
@@ -108,10 +115,12 @@ def main():
     raw_actions = get_data_from_csv("data_actions.csv")
     cleaned_actions = clean_data(raw_actions)
     actions_with_profits = calculate_profit(cleaned_actions)
-    actions_sorted = sort_actions_by_profit(actions_with_profits)
-    best_actions = get_best_actions(actions_sorted)
 
-    # best_actions = get_best_actions([], actions_with_profits, 0)
+    # actions_sorted = sort_actions_by_profit(actions_with_profits)
+    # best_actions = get_best_actions(actions_sorted)
+
+    best_actions = get_best_actions(actions_with_profits, max_budget=500)
+
 
     display_best_actions(best_actions)
 
